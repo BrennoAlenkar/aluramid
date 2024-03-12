15b112f5217ed76f3413f697e6b9947610e2ee92
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-03-12 16:49:13 -0300

    Atualizei

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
-}
-
-function tocaSomSplash() {
-    document.querySelector('#som_tecla_splash').play();
-}
-
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
