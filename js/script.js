69ea38d3e7a1ab7f4307f98eee67dbc3d4a4344b
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-03-18 16:29:28 -0300

    Atualizei

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
- let contador = 0;
-
- while(contador < listaDeTeclas.length) {
-
-    co
-
-    listaDeTeclas[0].onclick = function () {
-        tocaSom('#som_tecla_pom');
-    };
-    contador++;
- }
 
 
